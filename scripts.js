199878d01bfce333ee82f7f84f6b9d583ba22121
FidelisMarcelino <fidelis.binus@gmail.com>
2024-02-16 10:50:40 +0700

    third commit

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@ let button = document.getElementById('button');
 button.addEventListener('click', function(){
     alert("Message has Sent");
 })
+
+function toggleNavbar() {
+    const navList = document.querySelector('.navigate');
+    navList.classList.toggle('show');
+}
